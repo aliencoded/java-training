0964bcccf9dc8a062411baebce3b53e7a702cfbe
Abid,Ali <ali_abid@vanguard.com>
2018-04-04 11:37:50 -0400

    adding sockets initial

---
+++ src/main/java/com/aliencoded/java/core/training/threads/ThreadRunner.java
@@ -0,0 +1,28 @@
+/*
+ ****************************************************************************
+ *
+ * Copyright (c)2018 The Vanguard Group of Investment Companies (VGI)
+ * All rights reserved.
+ *
+ * This source code is CONFIDENTIAL and PROPRIETARY to VGI. Unauthorized
+ * distribution, adaptation, or use may be subject to civil and criminal
+ * penalties.
+ *
+ ****************************************************************************
+ Module Description:
+
+ $HeadURL:$
+ $LastChangedRevision:$
+ $Author:$
+ $LastChangedDate:$
+*/
+package com.aliencoded.java.core.training.threads;
+
+public class ThreadRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
